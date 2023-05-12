e6f862af6c3782a28f5be6ddc24bab38c8581496
Aki Inoyatov <info@gravityelements.com>
2023-05-12 10:05:31 -0700

    first step

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import questTheme from 'src/MyDesignSystemLightTheme';
+import NavBar from 'src/components/NavBar/NavBar';
+
+function App() {
+  return (
+    <HelmetProvider>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={questTheme}>
+          <Routes>
+            <Route path="/" element={<NavBar />} />
+          </Routes>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </HelmetProvider>
+  );
+}
+
+export default App;
